1aa52c0c40169817ce3b0d27d1e03a34e2a615d0
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-10-13 10:08:25 -0300

    chore: add drizzle setup with initial schema; add seed script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>teste</div>;
+  return (
+    <div>
+      teste
+      <Button>Button</Button>
+    </div>
+  );
 }
